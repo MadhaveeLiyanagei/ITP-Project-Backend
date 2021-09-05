4cc74453805814ad44be370f184b2b22837a5ff6
Madhavee Liyanagei <liyanageimadhavee@gmail.com>
2021-09-05 14:54:45 +0530

    First Commit

---
+++ src/main/java/com/itp/importexport/ItpProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.itp.importexport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItpProjectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItpProjectBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itp/importexport/ItpProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itp.importexport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItpProjectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
