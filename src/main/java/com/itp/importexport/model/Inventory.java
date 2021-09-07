5d4d939ffa048e77324a9624ddf63f111e29ac9b
shanalids <shanalids6@gmail.com>
2021-09-07 10:00:46 +0530

    first commit

--- src/main/java/com/itp/importexport/model/Inventory.java
+++ src/main/java/com/itp/importexport/model/Inventory.java
@@ -1,6 +1,7 @@
 package com.itp.importexport.model;
 
 import javax.persistence.Column;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
