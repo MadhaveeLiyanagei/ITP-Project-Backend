f00d6d75e68e011309b834dbe9441040637850e7
Ediri99 <bhanukaediri99@gmail.com>
2021-09-13 00:23:29 +0530

    Employee leaves crud operations

---
+++ src/main/java/com/itp/importexport/repository/EmployeeLeavesRepository.java
@@ -0,0 +1,9 @@
+package com.itp.importexport.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.itp.importexport.model.EmployeeLeaves;
+
+public interface EmployeeLeavesRepository extends JpaRepository <EmployeeLeaves, Integer>{
+
+}
